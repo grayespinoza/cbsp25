a6c51b60914b6d5ac6bf242598ea56a4ee911e77
Grey <grey@grayespinoza.dev>
2025-07-04 11:46:43 -0700

    feat: add MSS script

--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,27 @@
 # Cal-Bridge Summer Program
+
+## Usage
+
+### MSS
+> [!IMPORTANT]
+> MSS currently does not support Wayland, use X11 when running this script.
+
+Setup Virtual Environment
+```shell
+cd src/
+python -m venv mss
+source mss/bin/activate
+pip install opencv-python, mss
+```
+
+Use Script
+```shell
+cd src/
+source mss/bin/activate
+python mss/screenshot.py
+```
+
+Exit Virtual Environment
+```shell
+deactivate
+```
---
+++ src/mss/screenshot.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+import mss
+import numpy as np
+import os
+import time
+
+output_folder = "./mss/images"
+os.makedirs(output_folder, exist_ok=True)
+frame_count = 0
+
+with mss.mss() as sct:
+    while "Screenshotting":
+        last_time = time.time()
+
+        # Screenshot Monitor 2
+        img = np.array(sct.grab(sct.monitors[2]))
+
+        # Display
+        # cv.imshow("OpenCV/Numpy", img)
+
+        # Save
+        filename = os.path.join(output_folder, f"image_{frame_count:04d}.png")
+        cv.imwrite(filename, cv.cvtColor(img, cv.COLOR_BGRA2BGR))
+        frame_count += 1
+
+        print(f"Saved: {filename} FPS: {1 / (time.time() - last_time)}")
+
+        # Press "q" to quit.
+        # if cv.waitKey(25) & 0xFF == ord("q"):
+        #    cv.destroyAllWindows()
+        #    break
